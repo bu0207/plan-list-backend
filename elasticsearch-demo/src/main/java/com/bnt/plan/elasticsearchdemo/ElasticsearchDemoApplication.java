5f03f9e77162163df40979cadebff0d0caa7b792
bnt <49509592+bu0207@users.noreply.github.com>
2024-09-02 18:41:31 +0800

    es

---
+++ elasticsearch-demo/src/main/java/com/bnt/plan/elasticsearchdemo/ElasticsearchDemoApplication.java
@@ -0,0 +1,15 @@
+package com.bnt.plan.elasticsearchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchDemoApplication.class, args);
+        System.out.println("hello world");
+    }
+
+
+}
